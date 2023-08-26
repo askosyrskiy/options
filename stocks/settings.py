16db0e9849f0a7f5a1b6594a35f60865ad3751c7
Alexey Skosyrskiy <askosyrskiy@users.noreply.github.com>
2023-08-26 01:35:17 -0400

    Update settings.py

--- stocks/settings.py
+++ stocks/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -124,8 +124,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATICFILES_DIRS = [
-    BASE_DIR / "stocks/static"
+    BASE_DIR / "static"
 ]
 
 # Default primary key field type
